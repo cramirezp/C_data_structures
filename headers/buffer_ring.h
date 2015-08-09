0fd9cfa69069e50388750b8f7a4440672a8101fb
Cristóbal Ramírez <cristobal.ramirez@alumnos.usm.cl>
2015-08-09 13:31:55 -0300

    foreach looping

--- headers/buffer_ring.h
+++ headers/buffer_ring.h
@@ -7,29 +7,43 @@
 #define _BR_BUFFER_SIZE 100
 
 #define declare_bring(vtype, name) \
-	(struct _bf_t 						\
+	typedef struct						\
 	{									\
 		vtype buffer[_BR_BUFFER_SIZE];	\
 		uint16_t first, last;			\
-	}name)
+		uint8_t	size;					\
+	}_bf_t;								\
+	_bf_t name
 
-#define init_bring(vtype, name) \
-	{																	\
-		name.first = 0;													\
-		name.last   = 0;												\
-		memset((void*)&name.buffer, 0, sizeof(vtpye)*_BR_BUFFER_SIZE)	\
+
+#define init_bring(name) \
+	{																		\
+		name.first = 0;														\
+		name.last  = 0;														\
+		name.size  = 0;														\
+		memset((void*)&name.buffer, 0, sizeof(name.buffer)*_BR_BUFFER_SIZE);\
 	}
 
 #define push_bring(vtype, name, value) \
 		{																\
 			name.buffer[name.last++] = value;							\
 			name.last = (name.last < _BR_BUFFER_SIZE)? name.last: 0;	\
+			name.size++;												\
 		}
 #define pull_bring(vtype, name) \
 		(																\
 			name.buffer[name.first++],									\
-			name.first = (name.first < _BR_BUFFER_SIZE)? name.first: 0	\
+			name.first = (name.first < _BR_BUFFER_SIZE)? name.first: 0,	\
+			name.size = (name.size != 0)? name.size-1: 0				\
 		)
-
+/*
+#define foreach(vtype, name, it) \
+		int i_;						\
+		vtype it;					\
+		for(i_=name.first, it=name.buffer[name.first]; i_-1!=name.last; i_=(i_+1>=_BR_BUFFER_SIZE)? 0: i_+1, it=name.buffer[i_])
+*/
+#define foreach(vtype, name) \
+	int i_;					\
+	for(i_=name.first; i_-1!=name.last; i_=(i_+1>=_BR_BUFFER_SIZE)? 0: i_+1)
 
 #endif // _buffer_ring_h
--- src/buffer_ring.c
+++ src/buffer_ring.c
@@ -1,6 +1,19 @@
 #include "buffer_ring.h"
 
+//#define __USE_MINGW_ANSI_STDIO 1
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
+	declare_bring(int, x);
+	init_bring(x);
+
+	puts("Hola adasd\n\r");
+
+/*
+	foreach(int, x){
+		printf("Valor %d\n", i_);
+	}*/
+
 	return 0;
 }
